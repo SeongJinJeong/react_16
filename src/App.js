3564c093b45d910332045e327cea3c1bff75d1a4
SeongJin <jsj0718tjdwl@naver.com>
2020-02-19 22:30:33 +0900

    Portals!!!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React,{Component} from 'react';
+import {createPortal} from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+
+class Portals extends Component {
+  render(){
+    return createPortal(<Message />,document.getElementById("touchme"))
+  }
+}
+
+const Message = () =>{
+  return(
+    "Touched You!"
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Portals />
     </div>
   );
 }
